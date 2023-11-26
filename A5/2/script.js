29c15012097fe09a8631cb916514f83a099d0f74
SiJerryk <152089557+SiJerryk@users.noreply.github.com>
2023-11-27 04:14:32 +0700

    Add files via upload

---
+++ A5/2/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var petName = prompt("Привет! Как зовут твоего любимого питомца?");
+    alert("Привет, " + petName + "! Ты любишь своего питомца?");
+});
